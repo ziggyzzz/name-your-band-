0962635945051020616757d405b4cf709f61387d
ltf93 <36516220+ltf93@users.noreply.github.com>
2018-04-11 18:13:47 +0100

    environment variables

--- search.py
+++ search.py
@@ -1,1 +1,12 @@
+import os
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+os.environ["SPOTIPY_CLIENT_ID"] = "219a05249bd64963b75f8c7d32e9532c"
+os.environ["SPOTIPY_CLIENT_SECRET"] = "bda2343aed5c4be5a06e3b5bd3582a58"
+
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+artist=sp.search(q='yellow', limit=10, offset=0, type='artist', market='US')
+print(artist)
